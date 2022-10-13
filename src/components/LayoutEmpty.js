6abe5e0ccfdedbb7bdd27a1c9b1e58712db35ce9
roberto-carlos-tg <roberto.tg99@gmail.com>
2022-10-13 18:20:20 -0500

    feat: Add new components for user don´t have enterprise

---
+++ src/components/LayoutEmpty.js
@@ -0,0 +1,189 @@
+import React, { useEffect, useRef, useState } from "react";
+import classNames from "classnames";
+import { useLocation } from "react-router-dom";
+
+import { CSSTransition } from "react-transition-group";
+import { AppTopbar } from "../AppTopbar";
+import { AppFooter } from "../AppFooter";
+import { AppConfig } from "../AppConfig";
+import PrimeReact from "primereact/api";
+import { Tooltip } from "primereact/tooltip";
+
+const LayoutEmpty = ({ children }) => {
+  const [layoutMode, setLayoutMode] = useState("overlay");
+  const [layoutColorMode, setLayoutColorMode] = useState("light");
+  const [inputStyle, setInputStyle] = useState("outlined");
+  const [ripple, setRipple] = useState(true);
+  const [staticMenuInactive, setStaticMenuInactive] = useState(false);
+  const [overlayMenuActive, setOverlayMenuActive] = useState(false);
+  const [mobileMenuActive, setMobileMenuActive] = useState(false);
+  const [mobileTopbarMenuActive, setMobileTopbarMenuActive] = useState(false);
+  const copyTooltipRef = useRef();
+  const location = useLocation();
+
+  PrimeReact.ripple = true;
+
+  let menuClick = false;
+  let mobileTopbarMenuClick = false;
+
+  useEffect(() => {
+    if (mobileMenuActive) {
+      addClass(document.body, "body-overflow-hidden");
+    } else {
+      removeClass(document.body, "body-overflow-hidden");
+    }
+  }, [mobileMenuActive]);
+
+  useEffect(() => {
+    copyTooltipRef && copyTooltipRef.current &&
+      copyTooltipRef.current.updateTargetEvents();
+  }, [location]);
+
+  const onInputStyleChange = (inputStyle) => {
+    setInputStyle(inputStyle);
+  };
+
+  const onRipple = (e) => {
+    PrimeReact.ripple = e.value;
+    setRipple(e.value);
+  };
+
+  const onLayoutModeChange = (mode) => {
+    setLayoutMode(mode);
+  };
+
+  const onColorModeChange = (mode) => {
+    setLayoutColorMode(mode);
+  };
+
+  const onWrapperClick = (event) => {
+    if (!menuClick) {
+      setOverlayMenuActive(false);
+      setMobileMenuActive(false);
+    }
+
+    if (!mobileTopbarMenuClick) {
+      setMobileTopbarMenuActive(false);
+    }
+
+    mobileTopbarMenuClick = false;
+    menuClick = false;
+  };
+
+  const onToggleMenuClick = (event) => {
+    menuClick = true;
+
+    if (isDesktop()) {
+      if (layoutMode === "overlay") {
+        if (mobileMenuActive === true) {
+          setOverlayMenuActive(true);
+        }
+
+        setOverlayMenuActive((prevState) => !prevState);
+        setMobileMenuActive(false);
+      } else if (layoutMode === "static") {
+        setStaticMenuInactive((prevState) => !prevState);
+      }
+    } else {
+      setMobileMenuActive((prevState) => !prevState);
+    }
+
+    event.preventDefault();
+  };
+  const onMobileTopbarMenuClick = (event) => {
+    mobileTopbarMenuClick = true;
+
+    setMobileTopbarMenuActive((prevState) => !prevState);
+    event.preventDefault();
+  };
+
+  const onMobileSubTopbarMenuClick = (event) => {
+    mobileTopbarMenuClick = true;
+
+    event.preventDefault();
+  };
+
+  const isDesktop = () => {
+    return window.innerWidth >= 992;
+  };
+  const addClass = (element, className) => {
+    if (element.classList) {
+      element.classList.add(className);
+    } else {
+      element.className += " " + className;
+    }
+  };
+
+  const removeClass = (element, className) => {
+    if (element.classList) {
+      element.classList.remove(className);
+    } else {
+      element.className = element.className.replace(
+        new RegExp(
+          "(^|\\b)" + className.split(" ").join("|") + "(\\b|$)",
+          "gi",
+        ),
+        " ",
+      );
+    }
+  };
+
+  const wrapperClass = classNames("layout-wrapper", {
+    "layout-overlay": layoutMode === "overlay",
+    "layout-static": layoutMode === "static",
+    "layout-static-sidebar-inactive": staticMenuInactive &&
+      layoutMode === "static",
+    "layout-overlay-sidebar-active": overlayMenuActive &&
+      layoutMode === "overlay",
+    "layout-mobile-sidebar-active": mobileMenuActive,
+    "p-input-filled": inputStyle === "filled",
+    "p-ripple-disabled": ripple === false,
+    "layout-theme-light": layoutColorMode === "light",
+  });
+
+  return (
+    <div className={wrapperClass} onClick={onWrapperClick}>
+      <Tooltip
+        ref={copyTooltipRef}
+        target=".block-action-copy"
+        position="bottom"
+        content="Copied to clipboard"
+        event="focus"
+      />
+
+      <AppTopbar
+        onToggleMenuClick={onToggleMenuClick}
+        layoutColorMode={layoutColorMode}
+        mobileTopbarMenuActive={mobileTopbarMenuActive}
+        onMobileTopbarMenuClick={onMobileTopbarMenuClick}
+        onMobileSubTopbarMenuClick={onMobileSubTopbarMenuClick}
+      />
+
+      <div className="layout-main-container">
+        <div className="layout-main">{children}</div>
+        <AppFooter layoutColorMode={layoutColorMode} />
+      </div>
+      <AppConfig
+        rippleEffect={ripple}
+        onRippleEffect={onRipple}
+        inputStyle={inputStyle}
+        onInputStyleChange={onInputStyleChange}
+        layoutMode={layoutMode}
+        onLayoutModeChange={onLayoutModeChange}
+        layoutColorMode={layoutColorMode}
+        onColorModeChange={onColorModeChange}
+      />
+
+      <CSSTransition
+        classNames="layout-mask"
+        timeout={{ enter: 200, exit: 200 }}
+        in={mobileMenuActive}
+        unmountOnExit
+      >
+        <div className="layout-mask p-component-overlay"></div>
+      </CSSTransition>
+    </div>
+  );
+};
+
+export default LayoutEmpty;
